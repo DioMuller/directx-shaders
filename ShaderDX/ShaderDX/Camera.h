6223abb32cfbcc3efe9de81e68c04acbf4a67a58
Diogo Muller <diogo.muller@lactec.org.br>
2014-10-01 10:38:55 -0300

    Methods prototypes on Camera.

--- ShaderDX/ShaderDX/Camera.h
+++ ShaderDX/ShaderDX/Camera.h
@@ -1,8 +1,22 @@
 #pragma once
+#include <d3d9.h>
+
 class Camera
 {
-public:
-	Camera();
-	~Camera();
+	//////////////////////////////////////////
+	// Constructors
+	//////////////////////////////////////////
+	public:
+		Camera();
+		~Camera();
+
+	//////////////////////////////////////////
+	// Methods
+	//////////////////////////////////////////
+	public:
+		// Process whatever should be executed every turn.
+		virtual bool process(float time);
+		// Transforms the camera position.
+		void transform(IDirect3DDevice9* device);
 };
 
