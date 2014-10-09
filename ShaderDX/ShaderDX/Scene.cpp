86ae1d291294b2461ef7cf7fee6ea71420341737
Diogo Muller <diogo.muller@lactec.org.br>
2014-10-09 16:43:09 -0300

    Scene class without loading from XML.

--- ShaderDX/ShaderDX/Scene.cpp
+++ ShaderDX/ShaderDX/Scene.cpp
@@ -1,6 +1,8 @@
 #include "Scene.h"
 
-
+//////////////////////////////////////////
+// Constructors
+//////////////////////////////////////////
 Scene::Scene()
 {
 }
@@ -8,4 +10,67 @@ Scene::Scene()
 
 Scene::~Scene()
 {
+}
+
+//////////////////////////////////////////
+// Methods
+//////////////////////////////////////////
+
+// Setups the scene.
+void Scene::initialize(IDirect3DDevice9* device)
+{
+
+}
+
+// Process whatever should be executed every turn.
+bool Scene::process(float time)
+{
+	// Process Camera and Lights
+	camera->process(time);
+	light->process(time);
+
+	// Process Objects
+	for (auto object : objects)
+	{
+		object->process(time);
+	}
+
+	return true;
+}
+
+// Paints the scene on each loop.
+void Scene::paint(IDirect3DDevice9* device)
+{
+	// Clears the Scene.
+	HR(device->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(154, 206, 235), 1.0f, 0));
+
+	// Begins drawing the scene.
+	HR(device->BeginScene());
+
+	// Update camera transform and turn lights on.
+	camera->transform(device, shader);
+	light->paint(device, shader);
+
+	// Draw all the objects on the scene.
+	for (auto object : objects)
+	{
+		object->transformAndPaint(device, shader);
+	}
+
+	// Ends drawing the scene.
+	HR(device->EndScene());
+	// Presents scene / switches buffer.
+	HR(device->Present(NULL, NULL, NULL, NULL));
+}
+
+// Executed on end.
+void Scene::finish(IDirect3DDevice9* device)
+{
+
+}
+
+// Loads scene from an XML file.
+void Scene::loadFromFile(std::string path)
+{
+
 }
--- ShaderDX/ShaderDX/Scene.h
+++ ShaderDX/ShaderDX/Scene.h
@@ -1,8 +1,43 @@
 #pragma once
+#include <memory>
+
+#include "Mage/Effect.h"
+
+#include "Object.h"
+#include "Camera.h"
+#include "Light.h"
+
 class Scene
 {
-public:
-	Scene();
-	~Scene();
+	//////////////////////////////////////////
+	// Attributes
+	//////////////////////////////////////////
+	private:
+		mage::Effect* shader;
+		std::vector<std::shared_ptr<Object>> objects;
+		std::shared_ptr<Camera> camera;
+		std::shared_ptr<Light> light;
+
+	//////////////////////////////////////////
+	// Constructor
+	//////////////////////////////////////////
+	public:
+		Scene();
+		~Scene();
+
+	//////////////////////////////////////////
+	// Methods
+	//////////////////////////////////////////
+	public:
+		// Loads Scene from File.
+		void loadFromFile(std::string path);
+		// Setups the scene.
+		virtual void initialize(IDirect3DDevice9* device);
+		// Process whatever should be executed every turn.
+		virtual bool process(float time);
+		// Paints the scene on each loop.
+		virtual void paint(IDirect3DDevice9* device);
+		// Executed on end.
+		virtual void finish(IDirect3DDevice9* device);
 };
 
